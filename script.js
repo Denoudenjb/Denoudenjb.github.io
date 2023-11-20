43ac4107da79b32789c415cc555bf3e7da332172
Denoudenjb <88197149+Denoudenjb@users.noreply.github.com>
2023-11-19 18:53:31 -0600

    update

--- script.js
+++ script.js
@@ -22,45 +22,41 @@ $(window).on('scroll', function() {
     });
   });
   
-
-
-
-
-  
-  document.addEventListener('DOMContentLoaded', function () {
+  <script>
+  $(document).ready(function () {
     const cursor = document.querySelector('.cursor');
     const cursorInner = document.querySelector('.cursor-inner');
-  
+
     document.addEventListener('mousemove', function (e) {
       const x = e.clientX;
       const y = e.clientY;
-  
+
       cursor.style.left = x + 'px';
       cursor.style.top = y + 'px';
-  
+
       cursorInner.style.left = x + 'px';
       cursorInner.style.top = y + 'px';
     });
-  
+
     const handleMouseEnter = function () {
       cursor.style.width = '48px';
       cursor.style.height = '48px';
       cursorInner.style.width = '24px';
       cursorInner.style.height = '24px';
     };
-  
+
     const handleMouseLeave = function () {
       cursor.style.width = '32px';
       cursor.style.height = '32px';
       cursorInner.style.width = '16px';
       cursorInner.style.height = '16px';
     };
-  
+
     const links = document.querySelectorAll('a, button');
-  
+
     links.forEach(function (link) {
       link.addEventListener('mouseenter', handleMouseEnter);
       link.addEventListener('mouseleave', handleMouseLeave);
     });
   });
-  
+</script>
