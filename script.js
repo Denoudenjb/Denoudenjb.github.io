bd22580dfe9afd9b1b74901a986e4a7a6601eb5e
Denoudenjb <88197149+Denoudenjb@users.noreply.github.com>
2023-11-21 13:42:57 -0600

    test

--- script.js
+++ script.js
@@ -27,35 +27,41 @@ $(document).ready(function () {
   const cursor = document.querySelector('.cursor');
   const cursorInner = document.querySelector('.cursor-inner');
 
-  document.addEventListener('mousemove', function (e) {
-    const x = e.clientX;
-    const y = e.clientY;
+// ... Your existing JavaScript code ...
 
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
+document.addEventListener('mousemove', function (e) {
+  const x = e.clientX;
+  const y = e.clientY;
 
-    cursorInner.style.left = x + 'px';
-    cursorInner.style.top = y + 'px';
-  });
+  const cursor = document.querySelector('.cursor');
+  const cursorInner = document.querySelector('.cursor-inner');
 
-  const handleMouseEnter = function () {
-    cursor.style.width = '48px';
-    cursor.style.height = '48px';
-    cursorInner.style.width = '24px';
-    cursorInner.style.height = '24px';
-  };
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
 
-  const handleMouseLeave = function () {
-    cursor.style.width = '32px';
-    cursor.style.height = '32px';
-    cursorInner.style.width = '16px';
-    cursorInner.style.height = '16px';
-  };
-
-  const links = document.querySelectorAll('a, button');
-
-  links.forEach(function (link) {
-    link.addEventListener('mouseenter', handleMouseEnter);
-    link.addEventListener('mouseleave', handleMouseLeave);
-  });
+  cursorInner.style.left = x + 'px';
+  cursorInner.style.top = y + 'px';
 });
+
+const handleMouseEnter = function () {
+  cursor.style.width = '48px';
+  cursor.style.height = '48px';
+  cursorInner.style.width = '24px';
+  cursorInner.style.height = '24px';
+};
+
+const handleMouseLeave = function () {
+  cursor.style.width = '32px';
+  cursor.style.height = '32px';
+  cursorInner.style.width = '16px';
+  cursorInner.style.height = '16px';
+};
+
+const links = document.querySelectorAll('a, button');
+
+links.forEach(function (link) {
+  link.addEventListener('mouseenter', handleMouseEnter);
+  link.addEventListener('mouseleave', handleMouseLeave);
+});
+
+// ... Your existing JavaScript code ...
